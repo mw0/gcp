c15b6dd0bca56f595fb36a0c3db4b0eb6664aa98
MarkWilber <mwilberssl@gmail.com>
2015-10-12 10:26:04 -0700

    minor clean-up

--- code/test_AD_INF.py
+++ code/test_AD_INF.py
@@ -1,15 +1,31 @@
+#!/usr/bin/python
+
+
+# Simple script to test workings of caffe with AnomDetect.py and iForest.py. #
+
+
 import numpy as np
 import ImageNetFeaturizer as inf
 import AnomalyDetect as ad
 
-source_directory = '/home/wilber/work/caffe/examples/kitties/img'
-myINF = inf.ImageNetFeaturizer()
-myINF.preprocess_images(source_directory)
-X = myINF.featurize(fc_level = 7)
+#PROCESSED_FOLDER = '/home/wilber/work/Galvanize/gcp-data/iForest/cat'
+#PROCESSED_FOLDER = '/home/wilber/work/caffe/examples/kitties/img'
+#PROCESSED_FOLDER = '/home/wilber/work/Galvanize/gcp-data/iForest/pizza'
+#PROCESSED_FOLDER = '/home/wilber/work/Galvanize/gcp-data/iForest/tiger'
+PROCESSED_FOLDER = '/home/wilber/work/Galvanize/gcp/app/static'
+#PROCESSED_FOLDER = '/home/wilber/work/Galvanize/gcp-data/iForest/loonie'
+#PROCESSED_FOLDER = '/home/wilber/work/Galvanize/gcp-data/iForest/suburban_homes'
+
+INFmodel = inf.ImageNetFeaturizer()
+print "status: ", INFmodel.preprocess_images(PROCESSED_FOLDER)
+#X = INFmodel.featurize(fc_level = 7)
+X = INFmodel.featurize(fc_level = 6)
 print np.shape(X)
 
-myAD = ad.AnomalyDetect()
-myAD.X = X
-myAD.proc_files_list = myINF.src_file_list
-myAD.fit()
-myAD.show_top_k(10)
+ADmodel = ad.AnomalyDetect()
+ADmodel.X = X
+ADmodel.proc_file_list = INFmodel.src_file_list
+ADmodel.fit()
+print ADmodel.iFmodel.anomaly_score_
+print ""
+ADmodel.show_top_k(10)
