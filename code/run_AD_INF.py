9522f6585a5de53f0cd7a480e36c9dd879b44e10
MarkWilber <mwilberssl@gmail.com>
2015-11-22 16:52:33 -0800

    re-named test_AD_INF.py to run_AD_INF.py.

---
+++ code/run_AD_INF.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+
+# Simple script to test workings of caffe with AnomDetect.py and iForest.py. #
+
+
+import numpy as np
+import ImageNetFeaturizer as inf
+import AnomalyDetect as ad
+
+PROCESSED_FOLDER = '/home/wilber/work/Galvanize/gcp-data/iForest/tiger'
+
+INFmodel = inf.ImageNetFeaturizer()
+print "status: ", INFmodel.preprocess_images(PROCESSED_FOLDER)
+X = INFmodel.featurize(fc_level = 6)
+print np.shape(X)
+
+ADmodel = ad.AnomalyDetect()
+ADmodel.X = X
+ADmodel.proc_file_list = INFmodel.src_file_list
+ADmodel.fit()
+print ADmodel.iFmodel.anomaly_score_
+print ""
+ADmodel.show_top_k(10)
